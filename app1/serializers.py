7a1358285d58386178004c026f9f8e77e9bedbbc
shantanu kore <shantanukore2001@gmail.com>
2024-01-11 19:59:16 +0530

    my second commit

--- app1/serializers.py
+++ app1/serializers.py
@@ -6,6 +6,10 @@ class Studentserializer(serializers.Serializer):
     last_name=serializers.CharField(max_length=100)
     age=serializers.IntegerField()
 
+
+
+
+
     def create(self, validated_data):
         return Student.objects.create(**validated_data)
     def update(self, student, validated_data):
